23dd6de6768180a44d5bc8bcd5e85e1311723830
ekklezi <akleziast@gmail.com>
2023-01-10 17:12:24 +0300

    Chart JS implementation

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs';
 
+import Chart from 'chart.js/auto';
+
+import {DateTime} from 'luxon';
+import 'chartjs-adapter-luxon';
+
 window.Alpine = Alpine;
 
 Alpine.start();
@@ -14,3 +19,45 @@ axios.get('https://ipinfo.io')
         console.log(response);
       })
   });
+
+  if(document.getElementById('chart')){
+    console.log(123);
+    const ctx = document.getElementById('chart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            datasets: [{
+                label: 'Количество посетителей',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            indexAxis: 'y',
+            elements: {
+              bar: {
+                borderWidth: 2,
+              }
+            },
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'right',
+              },
+              title: {
+                display: true,
+                text: 'Количество посетителей за день'
+              }
+            },
+            scales: {
+                yield: {
+                    type: 'time',
+                    time: {
+                        unit: 'hour'
+                    }
+                }
+            }
+        },
+    });
+  }
